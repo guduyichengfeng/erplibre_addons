63a7ec55167db943599047fc1a98467211342fba
ziyu han <ziyu.han@technolibre.ca>
2023-04-04 09:57:47 -0400

    [UPD] aliment: utiliser read_event pour gérer la soumission de formulaire

--- aliment/static/src/js/website.aliment.animation.js
+++ aliment/static/src/js/website.aliment.animation.js
@@ -2,10 +2,42 @@ odoo.define("aliment.animation", function (require) {
     "use strict";
 
     let sAnimation = require("website.content.snippets.animation");
-
+    var ajax = require('web.ajax');
     sAnimation.registry.aliment = sAnimation.Class.extend({
         selector: ".o_aliment",
+        read_events: {
+            'submit #créer': 'créer',
+            'submit #modifier': 'modifier',
+        },
+        créer: function (ev) {
+            ev.preventDefault();  // prevent default form submission behavior
 
+            var name = this.$('#name').val();
+
+            ajax.jsonRpc('/créer', 'call', {
+              'name': name,
+            }).then(function (result) {
+              // handle the result of the RPC call here
+              //   console.log(result);
+                location.reload();
+            });
+
+        },
+        modifier: function (ev) {
+            ev.preventDefault();  // prevent default form submission behavior
+
+            var name = this.$('#new_name').val(); // get the new name of the aliment
+            var id = this.$('#aliment_id').val(); // get the ID of the aliment to be modified
+
+            ajax.jsonRpc('/modifier', 'call', {
+                'name': name,
+                'aliment_id': id, // pass the ID to the server
+            }).then(function (result) {
+                // handle the result of the RPC call here
+                //   console.log(result);
+                location.reload();
+            });
+        },
         start: function () {
             let self = this;
             this._eventList = this.$(".aliment_value");
@@ -21,13 +53,7 @@ odoo.define("aliment.animation", function (require) {
                     self._eventList.text("No data found");
                     return;
                 }
-
-
-                // self._$loadedContent = $(data);
-                // self._eventList.text(data["listes_aliments"]);
-
-                // This will create an unordered list (<ul>) element and append a list item (<li>) element for each item in the data.
-                // listes_aliments array. The resulting HTML will be added to the div element with the aliment_value class.
+                
                 var list = $("<ul>");
                 _.each(data.listes_aliments, function (item) {
                     var listIem = $("<li>");
@@ -36,37 +62,6 @@ odoo.define("aliment.animation", function (require) {
                     list.append(listIem);
                 });
                 self._eventList.html(list);
-            });
-            var ajax = require('web.ajax');
-
-            this.$('#créer').on('submit', function (ev) {
-                ev.preventDefault();  // prevent default form submission behavior
-
-                var name = $('#name').val();
-
-                ajax.jsonRpc('/créer', 'call', {
-                  'name': name,
-                }).then(function (result) {
-                  // handle the result of the RPC call here
-                  //   console.log(result);
-                    location.reload();
-                });
-            });
-            var ajax = require('web.ajax');
-            this.$('#modifier').on('submit', function (ev) {
-                ev.preventDefault();  // prevent default form submission behavior
-
-                var name = $('#new_name').val(); // get the new name of the aliment
-                var id = $('#aliment_id').val(); // get the ID of the aliment to be modified
-
-                ajax.jsonRpc('/modifier', 'call', {
-                    'name': name,
-                    'aliment_id': id, // pass the ID to the server
-                }).then(function (result) {
-                    // handle the result of the RPC call here
-                    //   console.log(result);
-                    location.reload();
-                });
             });
 
 
